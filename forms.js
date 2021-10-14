9aa8164c68191f529e6bc110cac27309ade15f17
Tristan Schmale <tristanschmale@Tristans-iMac.local>
2021-10-14 16:22:40 +0000

    updated style.css, added signin.html and forms.js

---
+++ forms.js
@@ -0,0 +1,43 @@
+//SIGN UP
+const signUp = e => {
+    let fname = document.getElementById('username').value,
+        email = document.getElementById('email').value,
+        pwd = document.getElementById('password').value;
+
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+    let exist = formData.length && 
+        JSON.parse(localStorage.getItem('formData')).some(data => 
+            data.fname.toLowerCase() == fname.toLowerCase()
+        );
+
+    if(!exist){
+        formData.push({ fname, email, pwd });
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.querySelector('form').reset();
+        document.getElementById('username').focus();
+        alert("Account Created.\n\nPlease Sign In using the link below.");
+        location.href = "signin.html";
+    }
+    else{
+        alert("Duplicate found!\nYou have already signed up");
+        document.getElementById("duplicate-msg").innerHTML = "Duplicate found, you have already signed up! <br> Please <a href='signin.html'>Sign In</a>.";
+    }
+    e.preventDefault();
+}
+
+//SIGN IN
+const signIn = e => {
+    let fname = document.getElementById('username').value, pwd = document.getElementById('password').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.fname.toLowerCase() == fname && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        // alert("Incorrect login credentials");
+        document.getElementById("incorrectLogin-msg").innerHTML = "Incorrect login credentials! <br> Please <a href='signup.html'>Sign up</a>.";
+    }
+    else{
+        location.href = "index.html";
+    }
+    e.preventDefault();
+}
